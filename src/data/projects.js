29fa490bc2b025c2cf77defb915986583021f49f
Madhankannan02 <madhankannan29@gmail.com>
2026-02-02 20:28:41 +0530

    Add 600px image variants for better responsive sizing

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,24 +1,28 @@
 
 // Titan Storage Imports
 import titanStorageHero400 from "../assets/images/general/Tital Storage hero-400.webp";
+import titanStorageHero600 from "../assets/images/general/Tital Storage hero-600.webp";
 import titanStorageHero800 from "../assets/images/general/Tital Storage hero-800.webp";
 import titanStorageHero1200 from "../assets/images/general/Tital Storage hero-1200.webp";
 import titanStoragePresentation from '../assets/images/general/Tital-Storage-Presentation.png';
 
 // Stract Imports
 import stractHero400 from '../assets/images/general/Stract Hero-400.webp';
+import stractHero600 from '../assets/images/general/Stract Hero-600.webp';
 import stractHero800 from '../assets/images/general/Stract Hero-800.webp';
 import stractHero1200 from '../assets/images/general/Stract Hero-1200.webp';
 import stractPresentation from '../assets/images/general/Stract Presentation.png';
 
 // Butcher Box Imports
 import butcherBoxHero400 from '../assets/images/general/butcherbox-projectcard-400.webp';
+import butcherBoxHero600 from '../assets/images/general/butcherbox-projectcard-600.webp';
 import butcherBoxHero800 from '../assets/images/general/butcherbox-projectcard-800.webp';
 import butcherBoxHero1200 from '../assets/images/general/butcherbox-projectcard-1200.webp';
 import butcherBoxPresentation from '../assets/images/general/Butcher Box Presentation.webp';
 
 // Hathaven Imports
 import hathaveHero400 from '../assets/images/general/hathaven-projectcard-400.webp';
+import hathaveHero600 from '../assets/images/general/hathaven-projectcard-600.webp';
 import hathaveHero800 from '../assets/images/general/hathaven-projectcard-800.webp';
 import hathaveHero1200 from '../assets/images/general/hathaven-projectcard-1200.webp';
 import hathavePresentation from '../assets/images/general/hathave presentation.jpg';
@@ -31,7 +35,7 @@ export const projects = [
         tags: ["UI/UX", "Web Design", "Figma"],
         image: {
             src: titanStorageHero800,
-            srcSet: `${titanStorageHero400} 400w, ${titanStorageHero800} 800w, ${titanStorageHero1200} 1200w`,
+            srcSet: `${titanStorageHero400} 400w, ${titanStorageHero600} 600w, ${titanStorageHero800} 800w, ${titanStorageHero1200} 1200w`,
             sizes: "(max-width: 640px) 90vw, (max-width: 1024px) 50vw, 568px"
         },
         link: titanStoragePresentation,
@@ -44,7 +48,7 @@ export const projects = [
         tags: ["Landing Page", "Theme", "Industrial Website"],
         image: {
             src: stractHero800,
-            srcSet: `${stractHero400} 400w, ${stractHero800} 800w, ${stractHero1200} 1200w`,
+            srcSet: `${stractHero400} 400w, ${stractHero600} 600w, ${stractHero800} 800w, ${stractHero1200} 1200w`,
             sizes: "(max-width: 640px) 90vw, (max-width: 1024px) 50vw, 568px"
         },
         link: stractPresentation,
@@ -57,7 +61,7 @@ export const projects = [
         tags: ["Ecommerce", "Website Design", "UI Design"],
         image: {
             src: butcherBoxHero800,
-            srcSet: `${butcherBoxHero400} 400w, ${butcherBoxHero800} 800w, ${butcherBoxHero1200} 1200w`,
+            srcSet: `${butcherBoxHero400} 400w, ${butcherBoxHero600} 600w, ${butcherBoxHero800} 800w, ${butcherBoxHero1200} 1200w`,
             sizes: "(max-width: 640px) 90vw, (max-width: 1024px) 50vw, 568px"
         },
         link: butcherBoxPresentation
@@ -69,7 +73,7 @@ export const projects = [
         tags: ["UX Research", "Case Study", "Ecommerce Website"],
         image: {
             src: hathaveHero800,
-            srcSet: `${hathaveHero400} 400w, ${hathaveHero800} 800w, ${hathaveHero1200} 1200w`,
+            srcSet: `${hathaveHero400} 400w, ${hathaveHero600} 600w, ${hathaveHero800} 800w, ${hathaveHero1200} 1200w`,
             sizes: "(max-width: 640px) 90vw, (max-width: 1024px) 50vw, 568px"
         },
         link: hathavePresentation,
